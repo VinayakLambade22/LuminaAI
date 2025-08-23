1f9a3fc166d7eace6c60cb5568a98b991bf0b7ed
Vinayak Lambade <vinayaklambade66593@gmail.com>
2025-08-23 23:01:55 +0530

    use VITE_BACKEND_URL for production and dev environment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.BACKEND_URL;
+export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 if (!API_BASE_URL) {
     throw new Error("API_BASE_URL is not set in environment variables");
